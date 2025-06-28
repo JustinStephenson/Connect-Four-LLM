31730ad08cff1df132bf2cc4d908a330ea0ede8a
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2025-06-28 16:32:44 -0700

    Adding more Ollama model names

--- backend/src/main/java/com/justinsoftware/connect_four_llm/llm/ollama/OllamaModel.java
+++ backend/src/main/java/com/justinsoftware/connect_four_llm/llm/ollama/OllamaModel.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum OllamaModel {
+    DeepSeek_r1("deepseek-r1"),
+    Gemma("gemma"),
+    Mistral("mistral"),
     LLama3_2("llama3.2");
 
     private final String modelName;
