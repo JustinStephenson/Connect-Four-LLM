7584c823d8ad70806516f328f5d103124c8e932f
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2025-07-24 17:24:50 -0700

    Adding reasoning text to cards

--- frontend/src/components/Game.vue
+++ frontend/src/components/Game.vue
@@ -3,7 +3,7 @@
     <div class="status">{{ status }}</div>
     <div class="view">
       <PlayerCard :image-url="Player_Icon" :token="players.PlayerOne">
-        <span>Player1</span>
+        <span>Player 1</span>
       </PlayerCard>
       <Board
         :slot-matrix="board"
@@ -16,7 +16,16 @@
           <br />
           <span>Thinking...</span>
         </div>
-        <div v-else>LLM</div>
+        <div v-else-if="llmResponse" class="response">
+          <div class="response__reason">
+            <span>Reasoning:</span>
+            <span>{{ llmResponse.reasoning }}</span>
+          </div>
+          <div class="response__choice">
+            Choice: {{ llmChoice(llmResponse.choice) }}
+          </div>
+        </div>
+        <div v-else>Player 2</div>
       </PlayerCard>
     </div>
   </section>
@@ -40,6 +49,7 @@ const players: Record<string, TokenType> = {
 const board = ref<TokenType[][]>(createMatrix(6, 7));
 const currentPlayer = ref<TokenType>(players.PlayerOne);
 const llmLoading = ref<boolean>(false);
+const llmResponse = ref<LLMResponse>(null);
 const status = ref<string>("");
 
 const onPlayerPlayed = (playerPos: PlayerPos) => {
@@ -68,9 +78,14 @@ const llmTakeTurn = async (boardState: TokenType[][]) => {
   });
   const response = await res.text();
   const jsonResponse = JSON.parse(response) as LLMResponse;
-  console.log(jsonResponse);
+
   llmLoading.value = false;
+  llmResponse.value = jsonResponse;
+
   placeToken(boardState, players.PlayerTwo, jsonResponse.choice);
+};
+const llmChoice = (choice: number): string => {
+  return "Column " + (choice + 1) + "";
 };
 </script>
 
@@ -92,5 +107,24 @@ const llmTakeTurn = async (boardState: TokenType[][]) => {
   display: grid;
   place-items: center;
   grid-template-columns: 1fr auto 1fr;
+}
+
+.response {
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 2fr 1fr;
+  justify-items: center;
+  align-items: center;
+  padding: 1.5rem;
+
+  &__reason {
+    @include mixin.flexCenter();
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  &__choice {
+  }
 }
 </style>
